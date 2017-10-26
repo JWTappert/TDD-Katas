9cfa0a46db7c7f9592b49aa785f708bb493af972
Justin Tappert <justin@theflybook.com>
2017-10-26 16:36:36 -0700

    Add project files.

---
+++ refactorfun-bowling/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refactorfun_bowling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
