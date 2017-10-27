3756443a9f8b639f146caa345829e0c3fde21111
Justin Tappert <justin@theflybook.com>
2017-10-26 17:15:08 -0700

    initial commit

--- refactorfun-bowling/Program.cs
+++ refactorfun-bowling/Program.cs
@@ -10,6 +10,40 @@ namespace refactorfun_bowling
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bowling!!!");
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+
+            var player1 = new Player(name);
+
+            Console.WriteLine($"Welcome, {player1.Name}. Get ready to bowl!");
+
+        }
+
+        
+    }
+
+    class Player
+    {
+        public string Name { get; set; } 
+        public int Score { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public int Bowl()
+        {
+            return RandomScoreCalculator.PinsDown();
+        }
+    }
+
+    class RandomScoreCalculator
+    {
+        public static int PinsDown()
+        {
+            return new Random().Next(1, 11);
         }
     }
 }
---
+++ refactorfun-bowling/tests/BowlTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Media;
+using NUnit.Framework;
+
+namespace refactorfun_bowling.tests
+{
+    [TestFixture]
+    public class BowlTests
+    {
+        private Player _player;
+
+        [SetUp]
+        public void Setup()
+        {
+            _player = new Player();
+        }
+
+        [Test]
+        public void SetsPlayerName()
+        {
+            
+        }
+    }
+}
