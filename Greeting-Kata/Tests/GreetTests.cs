b4b45265703b934c28cebbe6d8ed5d33bed155a2
Justin Tappert <justin@theflybook.com>
2018-02-13 07:32:24 -0800

    First tests passed

---
+++ Greeting-Kata/Tests/GreetTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace Greeting_Kata.Tests
+{
+    [TestFixture()]
+    public class GreetTests
+    {
+        private Greeter _greeter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _greeter = new Greeter();
+        }
+
+        [Test]
+        public void GreetMethodTest()
+        {
+           var response =  _greeter.Greet("Billy");
+
+            response.Should().Be("Hello, Billy");
+        }
+        
+    }
+
+    public class Greeter
+    {
+        public string Greet(string name)
+        {
+            return $"Hello, {name}";
+        }
+    }
+}
