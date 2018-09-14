f2eb5f072bc79de354c5968252b02f5378b4d383
JWTappert <justin@theflybook.com>
2018-09-13 20:06:16 -0700

    reset

--- refactorfun-bowling/tests/BowlTests.cs
+++ refactorfun-bowling/tests/BowlTests.cs
@@ -6,18 +6,11 @@ namespace refactorfun_bowling.tests
     [TestFixture]
     public class BowlTests
     {
-        private Player _player;
-
         [SetUp]
         public void Setup()
         {
-            _player = new Player("Justin");
         }
 
-        [Test]
-        public void SetsPlayerName()
-        {
-            
-        }
+        
     }
 }
